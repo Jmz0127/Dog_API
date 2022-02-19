a5be2221f1ae2a0dc3cba57cfffe43ab9b76256b
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-02-19 18:00:25 -0500

    set up basic express server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser'; //take in incoming POST request bodies
+
+const app = express(); //using express
+const PORT = 9000; //backend port
+
+app.use(bodyParser.json()) //initialize bodyParser called as a function
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))//listen for incoming requests
