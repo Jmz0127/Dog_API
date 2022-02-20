253187cae1c84ee768cdf49bb161536ae49591d8
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-02-20 11:50:59 -0500

    post requests with unique id add, can get id endpt

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-#Enjoy the API!!
+#Welcome to Dog_API, a simple CRUD App that renders a REST API. This database uses express, node, uuid (no two dogs will have the same id, similar to a veterinary system). On successful upload of a new dog, you will get a success message and unique Id generated.
+
+
+
+#Post your doggy's info with the three parameters - dogsName: breedType: age: - with POST endpoint "/"
+#Get pre-existing dogs with GET endpoint "/"
+
+
+
--- routes/dogs.js
+++ routes/dogs.js
@@ -1,33 +1,30 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid'; // id generator
+
 
 const router = express.Router();
 
 const dogs = [
-	{
-		dogsName: 'Roger',
-		breedType: 'Labrador Retriever',
-		age: '1.5 years old'
-	},
-	{
-		dogsName: 'Olive',
-		breedType: 'Golden Retriever',
-		age: '15 weeks old'
-	}
+	
 ];
 
 // all routes mentioned below start with /dogs - see index.js under usersRouter
 router.get('/', (req, res) => {
-    
     res.send(dogs);
 });
 
-router.post('/', (req, res) => {
-    
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
 
-    const dog = req.body
-    dogs.push(dog)
+    const foundDog = dogs.find((dog) => dog.id === id)
+    res.send(foundDog);
+});
+
+router.post('/', (req, res) => {
+    const dog = req.body;
+    dogs.push({ ...dog, id: uuidv4()});
    
     res.send(`Dog with the dog name of ${dog.dogsName} successfully added to the database. Woof!`);
-})
+});
 
 export default router;
